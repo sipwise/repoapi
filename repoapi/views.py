5918a694a9919084582e8977903a554f47369cee
Victor Seva <vseva@sipwise.com>
2015-04-13 22:41:27 +0200

    MT#7247 Initial commit
    Django App with a basic model for save JenkinsBuildInfo
    
    Change-Id: I8584d835db25f3f5db7d5d2fe072fdf1749e7dbe

---
+++ repoapi/views.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2015 The Sipwise Team - http://sipwise.com
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from repoapi import models, serializers
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'jenkinsbuildinfo': reverse('jenkinsbuildinfo-list',
+                                    request=request, format=format),
+    })
+
+
+class JenkinsBuildInfoList(generics.ListCreateAPIView):
+    queryset = models.JenkinsBuildInfo.objects.all()
+    serializer_class = serializers.JenkinsBuildInfoSerializer
+
+
+class JenkinsBuildInfoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.JenkinsBuildInfo.objects.all()
+    serializer_class = serializers.JenkinsBuildInfoSerializer
