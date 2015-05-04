d0e228d3f9f5bc7dfeb20f818d45cd99ea0722d6
Victor Seva <vseva@sipwise.com>
2015-05-04 15:06:12 +0200

    MT#7247 add panel app
    Change-Id: I2aae4609ac60fa0c54036ae4b09e9a4c48f29ea0

--- repoapi/urls.py
+++ repoapi/urls.py
@@ -36,4 +36,6 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^panel/', include('panel.urls',
+                            namespace='panel')),
 ]
