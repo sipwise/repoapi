b2ed94d516842b76eac55b481c40f12a9bad2a6b
Victor Seva <vseva@sipwise.com>
2015-05-08 10:56:46 +0200

    MT#7247 release.html: dynamic projects
    Change-Id: If773afc5a8e33c7cbb70fdb0ca3e3b7391653e26

--- panel/views.py
+++ panel/views.py
@@ -23,10 +23,7 @@ def index(request):
 
 
 def release(request, release):
-    context = {
-        'projects':  jbi.objects.release_projects(release),
-        'release': release
-    }
+    context = {'release': release }
     return render(request, 'panel/release.html', context)
 
 
