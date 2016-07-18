4418cf9b99a0cf8eb8a0b6d4802aa4b78f368106
Victor Seva <vseva@sipwise.com>
2016-07-18 13:55:46 +0200

    TT#1739
    Change-Id: I6d85dcec894d57f1a106335656bf6bc7a02b3a1a

--- repoapi/test/test_workfrontnote.py
+++ repoapi/test/test_workfrontnote.py
@@ -37,45 +37,39 @@ class WorkfrontNoteTestCase(TestCase):
         res = WorkfrontNoteInfo.getCommit("1234567 TT#67676 whatever")
         self.assertEquals(res, "1234567")
 
+    def get_defaults(self):
+        defaults = {
+            'tag': "edc90cd9-37f3-4613-9748-ed05a32031c2",
+            'projectname': "kamailio",
+            'jobname': "kamailio-gerrit",
+            'buildnumber': 897,
+            'result': "SUCCESS",
+            'job_url': "https://jenkins.mgm.sipwise.com/job/kamailio-gerrit/",
+            'gerrit_patchset': "1",
+            'gerrit_change': "2054",
+            'gerrit_eventtype': "patchset-created",
+            'param_tag': "none",
+            'param_branch': "master",
+            'param_release': "none",
+            'param_distribution': "wheezy",
+            'param_ppa': "gerrit_MT10339_review2054",
+            'git_commit_msg': "7fg4567 TT#0001 whatever",
+        }
+        return defaults
+
     @patch('repoapi.utils.workfront_note_send')
     def test_note_gerrit(self, utils):
-        JenkinsBuildInfo.objects.create(
-            tag="edc90cd9-37f3-4613-9748-ed05a32031c2",
-            projectname="kamailio",
-            jobname="kamailio-gerrit",
-            buildnumber=897,
-            result="SUCCESS",
-            job_url="https://jenkins.mgm.sipwise.com/job/kamailio-gerrit/",
-            gerrit_patchset="1",
-            gerrit_change="2054",
-            gerrit_eventtype="patchset-created",
-            param_tag="none",
-            param_branch="master",
-            param_release="none",
-            param_distribution="wheezy",
-            param_ppa="gerrit_MT10339_review2054")
+        param = self.get_defaults()
+        JenkinsBuildInfo.objects.create(**param)
 
         gri = WorkfrontNoteInfo.objects.filter(
             workfront_id="0001",
             gerrit_change="2054")
         self.assertEquals(gri.count(), 0)
 
-        JenkinsBuildInfo.objects.create(
-            tag="edc90cd9-37f3-4613-9748-ed05a32031c2",
-            projectname="kamailio",
-            jobname="kamailio-get-code",
-            buildnumber=897,
-            result="SUCCESS",
-            job_url="https://jenkins.mgm.sipwise.com/job/kamailio-get-code/",
-            gerrit_patchset="1",
-            gerrit_change="2054",
-            gerrit_eventtype="patchset-created",
-            param_tag="none",
-            param_branch="master",
-            param_release="none",
-            param_distribution="wheezy",
-            param_ppa="gerrit_MT10339_review2054",
-            git_commit_msg="7fg4567 TT#0001 whatever")
+        param['jobname'] = "kamailio-get-code"
+        param['buildnumber'] = 897
+        JenkinsBuildInfo.objects.create(**param)
 
         gri = WorkfrontNoteInfo.objects.filter(
             workfront_id="0001",
@@ -86,50 +80,44 @@ class WorkfrontNoteTestCase(TestCase):
 
     @patch('repoapi.utils.workfront_note_send')
     def test_note_merge(self, utils):
-        JenkinsBuildInfo.objects.create(
-            tag="edc90cd9-37f3-4613-9748-ed05a32031c2",
-            projectname="kamailio",
-            jobname="kamailio-gerrit",
-            buildnumber=897,
-            result="SUCCESS",
-            job_url="https://jenkins.mgm.sipwise.com/job/kamailio-gerrit/",
-            gerrit_patchset="1",
-            gerrit_change="2054",
-            gerrit_eventtype="change-merged",
-            param_tag="none",
-            param_branch="master",
-            param_release="none",
-            param_distribution="wheezy",
-            param_ppa="gerrit_MT10339_review2054")
+        param = self.get_defaults()
+        JenkinsBuildInfo.objects.create(**param)
 
         gri = WorkfrontNoteInfo.objects.filter(
             workfront_id="0001",
             gerrit_change="2054")
         self.assertEquals(gri.count(), 0)
 
-        JenkinsBuildInfo.objects.create(
-            tag="edc90cd9-37f3-4613-9748-ed05a32031c2",
-            projectname="kamailio",
-            jobname="kamailio-get-code",
-            buildnumber=897,
-            result="SUCCESS",
-            job_url="https://jenkins.mgm.sipwise.com/job/kamailio-get-code/",
-            gerrit_patchset="1",
-            gerrit_change="2054",
-            gerrit_eventtype="change-merged",
-            param_tag="none",
-            param_branch="master",
-            param_release="none",
-            param_distribution="wheezy",
-            param_ppa="gerrit_MT10339_review2054",
-            git_commit_msg="7fg456 TT#0001 whatever")
+        param['jobname'] = "kamailio-get-code"
+        param['buildnumber'] = 897
+        JenkinsBuildInfo.objects.create(**param)
 
         gri = WorkfrontNoteInfo.objects.filter(
             workfront_id="0001",
             gerrit_change="2054")
         self.assertEquals(gri.count(), 1)
-        msg = "review merged %s" % settings.GERRIT_URL.format("2054")
+        gri = WorkfrontNoteInfo.objects.filter(
+            workfront_id="0001",
+            gerrit_change="7fg4567")
+        self.assertEquals(gri.count(), 0)
+        msg = "review created %s" % settings.GERRIT_URL.format("2054")
         utils.assert_called_once_with("0001", msg)
+
+        param['jobname'] = "kamailio-get-code"
+        param['buildnumber'] = 898
+        param['gerrit_eventtype'] = "change-merged"
+        JenkinsBuildInfo.objects.create(**param)
+
+        gri = WorkfrontNoteInfo.objects.filter(
+            workfront_id="0001",
+            gerrit_change="2054")
+        self.assertEquals(gri.count(), 1)
+        gri = WorkfrontNoteInfo.objects.filter(
+            workfront_id="0001",
+            gerrit_change="7fg4567")
+        self.assertEquals(gri.count(), 1)
+        msg = "review merged %s" % settings.GERRIT_URL.format("2054")
+        utils.assert_called_with("0001", msg)
 
     @patch('repoapi.utils.workfront_note_send')
     def test_note_commit(self, utils):
