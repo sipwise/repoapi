sipwise/repoapi

commit 731dd69df0481d77ddc02ab46ee8be16f93200d3

TT#17622 build_docker
* provide info of actual images and tags
* refresh docker images/tag via release_dashboard
* keep that info in db
* fix Dockerfile documentation
* cleanup thanks to flake8

Change-Id: I743482da9b4d50f7b1832cad324882f3a49cbcf0

release_dashboard/tasks.py
@@ -16,9 +16,10 @@ from __future__ import absolute_import
 
 import logging
 from celery import shared_task
-from release_dashboard.models import Project
+from release_dashboard.models import Project, DockerImage, DockerTag
 from django.conf import settings
-from .utils import get_gerrit_tags, get_gerrit_branches
+from .utils import build
+from .utils import docker
 
 logger = logging.getLogger(__name__)
 rd_settings = settings.RELEASE_DASHBOARD_SETTINGS
@@ -27,12 +28,37 @@ rd_settings = settings.RELEASE_DASHBOARD_SETTINGS
 @shared_task(ignore_result=True)
 def gerrit_fetch_info(projectname):
     project, _ = Project.objects.get_or_create(name=projectname)
-    project.tags = get_gerrit_tags(projectname)
-    project.branches = get_gerrit_branches(projectname)
+    project.tags = build.get_gerrit_tags(projectname)
+    project.branches = build.get_gerrit_branches(projectname)
     project.save()
 
 
 @shared_task(ignore_result=True)
 def gerrit_fetch_all():
-    for project in rd_settings['projects']:
-        gerrit_fetch_info.delay(project)
+    for project in rd_settings['docker_projects']:
+        build.gerrit_fetch_info.delay(project)
+
+
+@shared_task(ignore_result=True)
+def docker_fetch_info(imagename):
+    image = DockerImage.objects.get(name=imagename)
+    tags = docker.get_docker_tags(imagename)
+    for tagname in tags:
+        DockerTag.objects.create(
+            name=tagname,
+            image=image,
+            manifests=docker.get_docker_manifests(image.name, tagname))
+
+
+@shared_task(ignore_result=True)
+def docker_fetch_all():
+    DockerImage.objects.all().delete()
+    images = docker.get_docker_repositories()
+    logger.debug("images: %s" % images)
+    for projectname in rd_settings['docker_projects']:
+        project, _ = Project.objects.get_or_create(name=projectname)
+        for imagename in project.filter_docker_images(images):
+            image = DockerImage.objects.create(name=imagename,
+                                               project=project)
+            logger.debug("%s created" % image)
+            docker_fetch_info.delay(image.name)

debian/server.ini
@@ -2,6 +2,7 @@
 JENKINS_URL=fake
 GERRIT_URL=fake
 BROKER_URL=fake
+DOCKER_REGISTRY_URL=fake
 DB_NAME=fake
 DB_USER=fake
 DB_PWD=fake

hotfix/test/test_hotfix_released.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.test import TestCase, override_settings
+from django.test import override_settings
 from mock import patch, call, mock_open
 from hotfix import tasks, utils, models
 from repoapi.models import JenkinsBuildInfo

release_dashboard/migrations/0002_dockerimage.py
@@ -1,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.9 on 2017-06-24 23:00
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+import django_extensions.db.fields.json
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('release_dashboard', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='DockerImage',
+            fields=[
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
+                ('name', models.CharField(max_length=50, unique=True)),
+                ('project', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='release_dashboard.Project')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='DockerTag',
+            fields=[
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
+                ('name', models.CharField(max_length=50)),
+                ('manifests', django_extensions.db.fields.json.JSONField()),
+                ('image', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='release_dashboard.DockerImage')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='dockertag',
+            unique_together=set([('name', 'image')]),
+        ),
+    ]

release_dashboard/models.py
@@ -15,6 +15,7 @@
 import logging
 import re
 import json
+from datetime import datetime
 from django.db import models
 from django_extensions.db.fields.json import JSONField
 from django_extensions.db.fields import ModificationDateTimeField
@@ -77,8 +78,61 @@ class Project(models.Model):
         return Project._filter_values(self.json_branches,
                                       '^refs/heads/(.+)$', regex)
 
+    def filter_docker_images(self, images):
+        r = re.compile(self.name)
+        return filter(r.search, images)
+
     def branches_mrXX(self):
         return self.filter_branches(r'^mr[0-9]+\.[0-9]+$')
 
     def branches_mrXXX(self):
         return self.filter_branches(r'^mr[0-9]+\.[0-9]+\.[0-9]+$')
+
+
+class DockerImageManager(models.Manager):
+
+    def images_with_tags(self):
+        qs = self.get_queryset().filter(dockertag__isnull=False)
+        return qs.distinct()
+
+
+class DockerImage(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    objects = DockerImageManager()
+
+    def __str__(self):
+        return "%s[%s]" % (self.name, self.project.name)
+
+    @property
+    def tags(self):
+        res = self.dockertag_set.all().values_list('name', flat=True)
+        return res
+
+
+class DockerTag(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    manifests = JSONField(null=False)
+    image = models.ForeignKey(DockerImage, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("name", "image"),)
+
+    def __str__(self):
+        return "%s:%s" % (self.image.name, self.name)
+
+    @property
+    def date(self):
+        if self.manifests is None:
+            return None
+        try:
+            value = self.manifests['history'][-1]['v1Compatibility']
+            time = json.loads(value)
+            created = time['created'].split('.')
+            return datetime.strptime(
+                created[0],
+                '%Y-%m-%dT%H:%M:%S')
+        except Exception as e:
+            logger.error(e)
+            return None

release_dashboard/serializers.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The Sipwise Team - http://sipwise.com
+# Copyright (C) 2017 The Sipwise Team - http://sipwise.com
 
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -13,12 +13,20 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.contrib import admin
+from rest_framework import serializers
 from . import models
 
 
-@admin.register(models.DockerTag)
-@admin.register(models.DockerImage)
-@admin.register(models.Project)
-class ProjectAdmin(admin.ModelAdmin):
-    pass
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Project
+        fields = '__all__'
+
+
+class DockerImageSerializer(serializers.HyperlinkedModelSerializer):
+    project = serializers.StringRelatedField()
+
+    class Meta:
+        model = models.DockerImage
+        fields = '__all__'

release_dashboard/static/release_dashboard/js/refresh_docker_info.js
@@ -1,0 +1,66 @@
+/**
+ * refresh info
+ */
+$('button#refresh_all').click(function(e){
+  // don't send the form
+  e.preventDefault();
+  var button = $(this);
+  var span = $('span#refresh_all_error');
+
+  $.ajax({
+    url: '/release_panel/docker/refresh/',
+    type: 'POST',
+    success: successFunc,
+    error: errorFunc,
+    beforeSend: csrftokenFunc
+  });
+
+  function successFunc(data, status) {
+    span.html('');
+    span.append('<a href="' + data.url + '">Done</a><br/>');
+    span.append('This will take a while. Refresh the page in a few');
+  }
+
+  function errorFunc(jqXHR, status, error) {
+    span.html(error);
+    button.removeAttr("disabled");
+  }
+
+  //deactivate button
+  button.attr("disabled", "disabled");
+  span.html('processing');
+  span.show();
+});
+
+$('button.refresh').click(function(e){
+  // don't send the form
+  e.preventDefault();
+  var button = $(this);
+  var project = button.attr('id').replace('refresh_','');
+  var span = $('span#refresh_error_' + project );
+
+  function successFunc(data, status) {
+    span.html('');
+    span.append('<a href="' + data.url + '">Done</a>');
+    button.removeAttr("disabled");
+  }
+
+  function errorFunc(jqXHR, status, error) {
+    span.html(error);
+    button.removeAttr("disabled");
+  }
+
+  $.ajax({
+    url: '/release_panel/docker/refresh/' + project + '/',
+    type: 'POST',
+    success: successFunc,
+    error: errorFunc,
+    beforeSend: csrftokenFunc
+  });
+
+  //deactivate button
+  button.attr("disabled", "disabled");
+  span.html('processing');
+  span.show();
+
+});

release_dashboard/templates/release_dashboard/build_docker_content.html
@@ -5,7 +5,6 @@
           <h3 class="panel-title">Actions</h3>
       </div>
       <div class="panel-body">
-{% if common_versions %}
         <div class="form-group">
             <label for="common_select">Common selection</label>
             <select class="form-control" id="common_select"
@@ -13,25 +12,6 @@
                 <option value="ignore">ignore</option>
             {% for v in common_versions.branches %}
                 <option value="branch/{{ v }}">branch/{{ v }}</option>
-            {% endfor %}
-            {% for v in common_versions.tags %}
-                <option value="tag/{{ v }}">tag/{{ v }}</option>
-            {% endfor %}
-            </select>
-        </div>
-{% endif %}
-        <div class="form-group">
-            <label for="version_release">Release version</label>
-            <input class="form-control" id="version_release"
-                name="version_release" type="text"
-                value="none">
-        </div>
-        <div class="form-group">
-            <label for="distribution">Debian release</label>
-            <select class="form-control" id="distribution"
-                name="distribution">
-            {% for d in debian %}
-                <option value="{{ d }}">{{ d }}</option>
             {% endfor %}
             </select>
         </div>
@@ -47,20 +27,20 @@
         <table class="table table-condensed">
           <thead>
               <tr>
-                  <th>Project</th>
-                  <th>Version</th>
+                  <th class="col-md-2">Project</th>
+                  <th class="col-md-2">Version</th>
               </tr>
           </thead>
           <tbody>
         {% for p in projects %}
             <tr class="repo">
-              <th><label for="version_{{ p.name }}">{{ p.name }}</label></th>
-              <td class="version">
-                  <select class="form-control" id="version_{{ p.name }}" name="version_{{ p.name }}">
+              <th class="col-md-2">
+                <label for="version_{{ p.name }}">{{ p.name }}</label>
+              </th>
+              <td class="version col-md-2">
+                  <select class="form-control select-version" id="version_{{ p.name }}" name="version_{{ p.name }}"
+                  project="{{ p.name }}">
                       <option value="ignore">ignore</option>
-                  {% for t in p.tags %}
-                      <option value="tag/{{ t }}">tag/{{ t }}</option>
-                  {% endfor %}
                   {% for b in p.branches %}
                       <option value="branch/{{ b }}">branch/{{ b }}</option>
                   {% endfor %}

commit b0333b8da6a703f36987a4c5454dc05f8dc3f4ca

TT#37390 Fix repoapi 'Refresh ALl' button.
It looks to be mistakenly broken in
731dd69df0481d77ddc02ab46ee8be16f93200d3

Fixing it back.

Change-Id: I34295422e840665f6f6ef153de3cc2085d0a0a95

release_dashboard/tasks.py
@@ -35,7 +35,7 @@ def gerrit_fetch_info(projectname):
 
 @shared_task(ignore_result=True)
 def gerrit_fetch_all():
-    for project in rd_settings['docker_projects']:
+    for project in rd_settings['projects']:
         gerrit_fetch_info.delay(project)
 
 