21d13b91af1fd556ee6da14e865b3bec313782b8
Michael Prokop <mprokop@sipwise.com>
2018-09-07 11:55:34 +0200

    TT#28953 Add buster to list of available Debian releases
    To be able to build trunk and its dependencies against
    Debian/buster we need to add it to the list of available
    Debian releases.
    
    Change-Id: I9bf840759c9cbcc1290a6d03761922b2a7b9fd6e

--- repoapi/settings/common.py
+++ repoapi/settings/common.py
@@ -152,7 +152,7 @@ CELERY_RESULT_BACKEND = 'django-db'
 HOTFIX_ARTIFACT = 'debian_changelog.txt'
 
 RELEASE_DASHBOARD_SETTINGS = {
-    'debian_supported': ('auto', 'stretch', 'jessie', 'wheezy', 'squeeze'),
+    'debian_supported': ('auto', 'buster', 'stretch', 'jessie', 'wheezy', 'squeeze'),
     'build_deps': (
         "check-tools", "data-hal", "libswrate", "sipwise-base", "mediaproxy-ng",
         "ngcp-schema", "rtpengine", "libtcap", "libinewrate"
