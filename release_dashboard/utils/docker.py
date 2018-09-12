18911597b9c310c22ad9c598710338f7ca975743
Manuel Montecelo <mmontecelo@sipwise.com>
2018-09-12 21:08:07 +0200

    TT#42659 More attempts to fix for "Delete" button in repoapi docker info page
    The previous changes help with deleting some of the unused tags (roughly half of
    the previous 30~40), but do not help in some cases and it's therefore impossible
    to remove the remaining ones.
    
    Currently, this is what happens:
    
    a) repoapi retrieves this url for a project, e.g. for a tag in data-hal-jessie:
       https://repoapi.mgm.sipwise.com/docker/tag/3534/ and uses tag.reference
    
       gets the "reference" attribute:
       sha256:fa18c06b84b17b19bed1d14b0b9d836bffba1531e68721dc8ad0a65cc7477761
    
    b) manage_docker.sh (which works correctly) uses this complex command:
    
       curl -vsiL -H "${curl_docker_header}" -X GET "${docker_url}/${docker_tag}" | sed -ne 's/^Docker-Content-Digest: \(sha256:[[[:alnum:]]\+\).*$/\1/ip;Tn;q;:n'
    
       and with this input:
    
       curl -vsiL -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -X GET \
            'https://docker.mgm.sipwise.com:5000/v2/data-hal-jessie/manifests/I30522e47cadb23b042e48f23b5a509dc9abf6ccb'
    
       gets as reference:
       sha256:ebc37e95c8844d37ec174d6208f80d664a5d099802d839cde0a4f74424d317fe
    
    c) from stackoverflow in
       https://stackoverflow.com/questions/38795240/delete-image-from-docker-registry-v2
       it is suggested to take json.config.digest of the output of the url above (instead
       of headers, like manage_docker.sh), and that is:
    
       "config": {
         "mediaType": "application/vnd.docker.container.image.v1+json",
         "size": 4915,
         "digest": "sha256:fac5e71f2f690379d5fad98c550cb451a8c5197f529e519adaab7a66cef00019"
       },
    
       which is again different from both values above.
    
    So this change adds code to remove in a way that mimics case b), in addition to
    the existing one, in another attempt to completely fix the issue.
    
    (It is not very elegant and it would be nice to pinpoint the exact problem
    without these trial and error cycles by pushing to production, but it's
    difficult when it depends on a running environment and the tools are not well
    known to us, e.g. Django.)
    
    Change-Id: I46859e369f039fffa3e191afb92c60df77d7e6e5

--- release_dashboard/utils/docker.py
+++ release_dashboard/utils/docker.py
@@ -136,7 +136,19 @@ def get_docker_manifests(image, tag):
             return (None, None)
 
 
-def delete_tag(image, reference):
+def delete_tag(image, reference, tag_name):
     dru = settings.DOCKER_REGISTRY_URL
-    url = dru.format("%s/manifests/%s" % (image, reference))
+
+    try:
+        url = dru.format("%s/manifests/%s" % (image, reference))
+        delete_docker_info(url)
+    except:
+        pass
+
+    # it does not work for some, retrieve Docker-Content-Digest from manifests
+    # and delete using that as reference
+    url = dru.format("%s/manifests/%s" % (image, tag_name))
+    response = get_docker_manifests_info(url)
+
+    url = dru.format("%s/manifests/%s" % (image, response[1]))
     delete_docker_info(url)
