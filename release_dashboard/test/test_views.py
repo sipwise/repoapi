d7fe37cf55a84da1d86c597a65d7932949e16ee0
Victor Seva <vseva@sipwise.com>
2020-02-07 13:56:23 +0100

    TT#19230 require that user is logged in for hotfix views
    Change-Id: Ib6bb8acdbdd2ee3a92d546f32e4b2527ae1f79fb

---
+++ release_dashboard/test/test_views.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2020 The Sipwise Team - http://sipwise.com
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this prograproj.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestHotfix(TestCase):
+    def test_no_login(self):
+        res = self.client.get(reverse("release_dashboard:hotfix"))
+        self.assertNotEqual(res.status_code, 200)
+
+    def test_login_ok(self):
+        user = User.objects.create_user(username="test")
+        self.client.force_login(user)
+        res = self.client.get(reverse("release_dashboard:hotfix"))
+        self.assertEqual(res.status_code, 200)
