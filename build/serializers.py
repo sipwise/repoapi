ba478571f2fdb354c7a3823cccb93baaf51777ff
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers relase-copy-debs-yml job first
    * remove links to build_deps or build as a form of list of projects
    * add some examples of config.yml releases for testing
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- build/serializers.py
+++ build/serializers.py
@@ -1,31 +1,42 @@
 # Copyright (C) 2017 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from rest_framework import serializers
+
+from . import exceptions as err
 from . import models
+from .utils import ReleaseConfig
 
 
 class BuildReleaseSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = models.BuildRelease
-        fields = '__all__'
+        fields = "__all__"
+        read_only_fields = ["tag", "branch", "distribution", "projects"]
 
-    def validate_projects(self, value):
-        projects = [x.strip() for x in value.split(',')]
-        if len(projects) <= 0:
+    def validate_release(self, value):
+        try:
+            self.release_config = ReleaseConfig(value)
+        except err.NoConfigReleaseFile:
+            raise serializers.ValidationError("{} unknown".format(value))
+        except err.NoJenkinsJobsInfo:
             raise serializers.ValidationError(
-                "projects is not a list of coma separate elements")
-        return ','.join(projects)
+                "{} has no jenkins-job info".format(value)
+            )
+        return self.release_config.release
+
+    def create(self, validate_data):
+        return self.Meta.model.objects.create_build_release(
+            validate_data["uuid"], validate_data["release"]
+        )
