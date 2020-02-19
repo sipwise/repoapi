0fd2b23b2c8b0f214b9a2b7794c8f0d8cf47f1c9
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers relase-copy-debs-yml job first
    * remove links to build_deps or build as a form of list of projects
    * add some examples of config.yml releases for testing
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- repoapi/settings/prod.py
+++ repoapi/settings/prod.py
@@ -1,29 +1,33 @@
 # Copyright (C) 2015 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#
+# Build paths inside the project like this: join(BASE_DIR, ...)
 import os
 from configparser import RawConfigParser
-# pylint: disable=W0401,W0614
-from .common import *
+from os.path import dirname
+from os.path import join
+
 from celery.schedules import crontab
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from .common import *  # noqa
 
-VAR_DIR = '/var/lib/repoapi'
+# pylint: disable=W0401,W0614
+
+BASE_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
+
+VAR_DIR = "/var/lib/repoapi"
 if not os.path.exists(VAR_DIR):
     VAR_DIR = BASE_DIR
 
@@ -32,25 +36,28 @@ if not os.path.exists(VAR_DIR):
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # read it from external file
-SECRET_KEY = open(os.path.join(VAR_DIR, '.secret_key')).read().strip()
+SECRET_KEY = open(join(VAR_DIR, ".secret_key")).read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.mgm.sipwise.com']
+ALLOWED_HOSTS = [".mgm.sipwise.com"]
 
-INSTALLED_APPS.extend(PROJECT_APPS)
+INSTALLED_APPS.extend(PROJECT_APPS)  # noqa
 
-LOGGING['loggers']['repoapi']['level'] = os.getenv('DJANGO_LOG_LEVEL', 'INFO')
+LOGGING["loggers"]["repoapi"]["level"] = os.getenv(  # noqa
+    "DJANGO_LOG_LEVEL", "INFO"
+)  # noqa
 
 server_config = RawConfigParser()
-server_config.read(os.path.join(VAR_DIR, 'server.ini'))
-JENKINS_URL = server_config.get('server', 'JENKINS_URL')
-GERRIT_URL = server_config.get('server', 'GERRIT_URL')
-DOCKER_REGISTRY_URL = server_config.get('server', 'DOCKER_REGISTRY_URL')
-AUTH_LDAP_SERVER_URI = server_config.get('server', 'AUTH_LDAP_SERVER_URI')
-AUTH_LDAP_USER_BASE = server_config.get('server', 'AUTH_LDAP_USER_BASE')
-AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,' + AUTH_LDAP_USER_BASE
+server_config.read(join(VAR_DIR, "server.ini"))
+JENKINS_URL = server_config.get("server", "JENKINS_URL")
+GERRIT_URL = server_config.get("server", "GERRIT_URL")
+DOCKER_REGISTRY_URL = server_config.get("server", "DOCKER_REGISTRY_URL")
+AUTH_LDAP_SERVER_URI = server_config.get("server", "AUTH_LDAP_SERVER_URI")
+AUTH_LDAP_USER_BASE = server_config.get("server", "AUTH_LDAP_USER_BASE")
+AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s," + AUTH_LDAP_USER_BASE
+BUILD_POOL = server_config.get("server", "BUILD_POOL")
 
 # Keep ModelBackend around for per-user permissions and maybe a local
 # superuser.
@@ -63,48 +70,48 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': server_config.get('server', 'DB_NAME'),
-        'USER': server_config.get('server', 'DB_USER'),
-        'PASSWORD': server_config.get('server', 'DB_PWD'),
-        'HOST': 'localhost',
-        'PORT': '',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": server_config.get("server", "DB_NAME"),
+        "USER": server_config.get("server", "DB_USER"),
+        "PASSWORD": server_config.get("server", "DB_PWD"),
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
 gerrit_config = RawConfigParser()
-gerrit_config.read(os.path.join(VAR_DIR, 'gerrit.ini'))
-GERRIT_REST_HTTP_USER = gerrit_config.get('gerrit', 'HTTP_USER')
-GERRIT_REST_HTTP_PASSWD = gerrit_config.get('gerrit', 'HTTP_PASSWD')
+gerrit_config.read(join(VAR_DIR, "gerrit.ini"))
+GERRIT_REST_HTTP_USER = gerrit_config.get("gerrit", "HTTP_USER")
+GERRIT_REST_HTTP_PASSWD = gerrit_config.get("gerrit", "HTTP_PASSWD")
 
 GITWEB_URL = "https://git.mgm.sipwise.com/gitweb/?p={}.git;a=commit;h={}"
-WORKFRONT_CREDENTIALS = os.path.join(BASE_DIR,
-                                     '/etc/jenkins_jobs/workfront.ini')
+WORKFRONT_CREDENTIALS = join(BASE_DIR, "/etc/jenkins_jobs/workfront.ini")
 WORKFRONT_NOTE = True
 
 # build app
 BUILD_KEY_AUTH = True
+REPOS_SCRIPTS_CONFIG_DIR = "/usr/share/sipwise-repos-scripts/config"
 
 # celery
-BROKER_URL = server_config.get('server', 'BROKER_URL')
+BROKER_URL = server_config.get("server", "BROKER_URL")
 CELERYBEAT_SCHEDULE = {
     # Executes every Sunday morning at 7:30 A.M
-    'purge-trunk': {
-        'task': 'repoapi.tasks.jbi_purge',
-        'schedule': crontab(hour=7, minute=30, day_of_week='sunday'),
-        'args': ('none', 4),
+    "purge-trunk": {
+        "task": "repoapi.tasks.jbi_purge",
+        "schedule": crontab(hour=7, minute=30, day_of_week="sunday"),
+        "args": ("none", 4),
     },
-    'purge-none': {
-        'task': 'repoapi.tasks.jbi_purge',
-        'schedule': crontab(hour=7, minute=30, day_of_week='sunday'),
-        'args': (None, 1),
+    "purge-none": {
+        "task": "repoapi.tasks.jbi_purge",
+        "schedule": crontab(hour=7, minute=30, day_of_week="sunday"),
+        "args": (None, 1),
     },
 }
-CELERY_TIMEZONE = 'UTC'
+CELERY_TIMEZONE = "UTC"
 
-JBI_BASEDIR = os.path.join(VAR_DIR, 'jbi_files')
+JBI_BASEDIR = join(VAR_DIR, "jbi_files")
 JBI_ARTIFACT_JOBS = [
-    'release-tools-runner',
+    "release-tools-runner",
 ]
-JBI_ALLOWED_HOSTS = ['jenkins.mgm.sipwise.com']
+JBI_ALLOWED_HOSTS = ["jenkins.mgm.sipwise.com"]
