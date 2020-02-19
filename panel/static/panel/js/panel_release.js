432372da62021ba636fa3178729afef6923a4a50
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers settings.RELEASE_JOBS jobs first
    * old build process on hidden /release_panel/old/
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    * rework panel.js to support both views /release/ and /release_uuid/
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

---
+++ panel/static/panel/js/panel_release.js
@@ -0,0 +1,112 @@
+/* eslint-disable-next-line no-unused-vars*/ // used at onClick
+function click_retrigger( e, project ) {
+    retrigger_project( project );
+    e.preventDefault();
+}
+
+function retrigger_project( project ) {
+  var div_project = $( "#" + project );
+
+  function successFunc( _data, _textStatus, _jqXHR ) {
+    /* eslint-disable-next-line no-undef */ // on panel.js
+    showProjectBuild( div_project, false );
+    $( "#" + project + "-error" ).text();
+  }
+
+  function errorFunc( _jqXHR, _status, error ) {
+    $( "#" + project + "-error" ).text( error );
+  }
+  var csrftoken = jQuery( "[name=csrfmiddlewaretoken]" ).val();
+  function csrfSafeMethod( method ) {
+
+    // these HTTP methods do not require CSRF protection
+    return ( /^(GET|HEAD|OPTIONS|TRACE)$/.test( method ) );
+  }
+  $.ajaxSetup( {
+    beforeSend: function( xhr, settings ) {
+        if ( !csrfSafeMethod( settings.type ) && !this.crossDomain ) {
+            xhr.setRequestHeader( "X-CSRFToken", csrftoken );
+        }
+    }
+  } );
+  var url = "/build/" + $.release.uuid + "/" + project + "/?format=json";
+  $.ajax( {
+    async: true,
+    url: url,
+    method: "POST",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: successFunc,
+    error: errorFunc
+  } );
+}
+
+function create_new_release_job( data ) {
+  var job = data.jobname;
+  if ( $.release.release_jobs.has( job ) ) {
+    return;
+  }
+
+  $.release.release_jobs.add( job );
+  $.release[ job ] = { uuids: new Set(), failed: false,
+    interval: 5000, last_uuid: null, removed_uuids: new Set() };
+
+  var div_job = $( ".clone-release-job" ).clone();
+  div_job.removeClass( "hidden clone-release-job" );
+  div_job.attr( "id", job );
+  div_job.html( "<a href=\"" + data.job_url + data.buildnumber + "\">" + job + "</a>" );
+  /* eslint-disable-next-line no-undef */ //defined at panel.js file
+  div_job.addClass( get_class_status( "list-group-item-", data.result ) );
+  div_job.appendTo( "#release-job-list" );
+}
+
+function get_release_jobs_info( job ) {
+
+  function successFunc( data, _textStatus, _jqXHR ) {
+    $( data ).each( function() {
+      if ( !$.release.release_jobs.has( this.jobname ) ) {
+        create_new_release_job( this );
+      }
+    } );
+  }
+
+  function errorFunc( _jqXHR, _status, error ) {
+    console.error( error );
+  }
+
+  var url = "/release_jobs/" + $.release.uuid + "/" + job + "/?format=json";
+  $.ajax( {
+    async: true,
+    url: url,
+    method: "GET",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: successFunc,
+    error: errorFunc
+  } );
+}
+
+/* eslint-disable-next-line no-unused-vars */ // used on templates
+function get_release_jobs() {
+
+  function successFunc( data, _textStatus, _jqXHR ) {
+    $( data ).each( function() {
+      get_release_jobs_info( this );
+    } );
+  }
+
+  function errorFunc( _jqXHR, _status, error ) {
+    console.error( error );
+  }
+
+  var url = "/release_jobs/" + $.release.uuid + "/?format=json";
+  $.ajax( {
+    async: true,
+    url: url,
+    method: "GET",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: successFunc,
+    error: errorFunc
+  } );
+}
