01a3d2fea8201d64a0a55ebf534a10add6f6b479
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers settings.RELEASE_JOBS jobs first
    * old build process on hidden /release_panel/old/
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    * rework panel.js to support both views /release/ and /release_uuid/
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- repoapi/settings/common.py
+++ repoapi/settings/common.py
@@ -1,18 +1,18 @@
 # Copyright (C) 2015 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from os.path import dirname
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django_assets",
     "django_celery_results",
     "django_extensions",
+    "django_filters",
     "jsonify",
 ]
 
@@ -128,10 +129,6 @@ LOGGING = {
             "handlers": ["console"],
             "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
         },
-        "release_dashboard": {
-            "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
-        },
     },
 }
 
@@ -151,6 +148,14 @@ DEBIAN_RELEASES = (
     "wheezy",
     "squeeze",
 )
+# sipwise-repos-scripts config files we don't want
+RELEASES_SKIP = [
+    "internal",
+    "mr0.1",
+]
+RELEASE_JOBS = [
+    "release-copy-debs-yml",
+]
 RELEASE_DASHBOARD_SETTINGS = {
     "debian_supported": DEBIAN_RELEASES,
     "build_deps": (
