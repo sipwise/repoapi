a8b473b103a40e3dfbd66b51b8e648414d7d500c
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers release-copy-debs-yml job first
    * remove links to build_deps or build as a form of list of projects
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- build/views.py
+++ build/views.py
@@ -1,31 +1,38 @@
 # Copyright (C) 2017 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from django.conf import settings
 import django_filters
+from django.conf import settings
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_api_key.permissions import HasAPIAccess
-from . import models, serializers
+
+from . import models
+from . import serializers
+from . import tasks
+from repoapi.serializers import JenkinsBuildInfoSerializer as JBISerializer
 
 
 class BuildReleaseFilter(django_filters.FilterSet):
-
     class Meta:
         model = models.BuildRelease
-        fields = ['release', 'start_date']
-        order_by = ['-start_date', ]
+        fields = ["release", "start_date"]
+        order_by = [
+            "-start_date",
+        ]
 
 
 class BuildReleaseList(generics.ListCreateAPIView):
@@ -39,3 +46,41 @@ class BuildReleaseList(generics.ListCreateAPIView):
 class BuildReleaseDetail(generics.RetrieveAPIView):
     queryset = models.BuildRelease.objects.all()
     serializer_class = serializers.BuildReleaseSerializer
+
+
+class BuildProject(APIView):
+    def get(self, request, release_uuid, project):
+        br = get_object_or_404(models.BuildRelease, uuid=release_uuid)
+        tasks.build_project.delay(br.id, project)
+        return Response({})
+
+
+class ReleaseJobs(APIView):
+    def get(self, request, release_uuid):
+        br = get_object_or_404(models.BuildRelease, uuid=release_uuid)
+        res = models.BuildRelease.objects.release_jobs(br.uuid)
+        if res is None:
+            return Response([])
+        return Response(res)
+
+
+class ReleaseJobsFull(APIView):
+    def get(self, request, release_uuid):
+        br = get_object_or_404(models.BuildRelease, uuid=release_uuid)
+        res = models.BuildRelease.objects.release_jobs_full(br.uuid)
+        if res is None:
+            return Response([])
+        return Response(res)
+
+
+class ReleaseJobsUUID(APIView):
+    def get(self, request, release_uuid, job):
+        br = get_object_or_404(models.BuildRelease, uuid=release_uuid)
+        jbis = models.BuildRelease.objects.release_jobs_uuids(br.uuid, job)
+        if jbis is None:
+            return Response([])
+        res = list()
+        for jbi in jbis:
+            serializer = JBISerializer(jbi, context={"request": request})
+            res.append(serializer.data)
+        return Response(res)
