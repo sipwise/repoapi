f53fbd0087d1a27205e45f1886a8936a23bedd3b
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers settings.RELEASE_JOBS jobs first
    * old build process on hidden /release_panel/old/
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    * rework panel.js to support both views /release/ and /release_uuid/
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- build/models/br.py
+++ build/models/br.py
@@ -1,21 +1,74 @@
 # Copyright (C) 2017 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 import logging
+
+from django.conf import settings
 from django.db import models
+from django.forms.models import model_to_dict
+
+from build.utils import ReleaseConfig
+from repoapi.models import JenkinsBuildInfo
 
 logger = logging.getLogger(__name__)
+
+
+class BuildReleaseManager(models.Manager):
+    jbi = JenkinsBuildInfo.objects
+
+    def create_build_release(self, uuid, release):
+        config = ReleaseConfig(release)
+        return self.create(
+            uuid=uuid,
+            tag=config.tag,
+            branch=config.branch,
+            release=config.release,
+            distribution=config.debian_release,
+            projects=",".join(config.projects),
+        )
+
+    def release_jobs(self, release_uuid, flat=True):
+        qs = self.jbi.get_queryset()
+        res = qs.filter(
+            jobname__in=settings.RELEASE_JOBS, param_release_uuid=release_uuid,
+        ).distinct()
+        if res.exists():
+            if flat:
+                return res.values_list("jobname", flat=True)
+            else:
+                return res.values("jobname")
+
+    def release_jobs_full(self, release_uuid):
+        res = dict()
+        jobs = self.release_jobs(release_uuid)
+        if jobs is None:
+            return res
+        for job in jobs:
+            uuids = self.release_jobs_uuids(release_uuid, job)
+            res[job] = [model_to_dict(x) for x in uuids]
+        return res
+
+    def release_jobs_uuids(self, release_uuid, job):
+        qs = self.jbi.get_queryset()
+        params = {
+            "param_release_uuid": release_uuid,
+            "jobname": job,
+            "tag__isnull": False,
+        }
+        res = qs.filter(**params).distinct()
+        if res.exists():
+            return res.order_by("-date")
 
 
 class BuildRelease(models.Model):
@@ -23,14 +76,107 @@ class BuildRelease(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     tag = models.CharField(max_length=50, null=True, blank=True)
     branch = models.CharField(max_length=50, null=False)
-    release = models.CharField(max_length=50, null=False,
-                               db_index=True)
-    distribution = models.CharField(max_length=50, null=False)
-    projects = models.TextField(null=False)
+    release = models.CharField(max_length=50, null=False, db_index=True)
+    distribution = models.CharField(max_length=50, null=False, editable=False)
+    projects = models.TextField(null=False, editable=False)
+    built_projects = models.TextField(null=True, editable=False)
+    failed_projects = models.TextField(null=True, editable=False)
+    objects = BuildReleaseManager()
 
     def __str__(self):
         return "%s[%s]" % (self.release, self.uuid)
 
     @property
     def projects_list(self):
-        return [x.strip() for x in self.projects.split(',')]
+        return [x.strip() for x in self.projects.split(",")]
+
+    @property
+    def built_projects_list(self):
+        if self.built_projects is not None:
+            return [x.strip() for x in self.built_projects.split(",")]
+        return []
+
+    @property
+    def queued_projects_list(self):
+        return [
+            x for x in self.projects_list if x not in self.built_projects_list
+        ]
+
+    def _append_falied(self, value):
+        if self.failed_projects is None:
+            self.failed_projects = value
+        else:
+            self.failed_projects += ",{}".format(value)
+        self.save()
+
+    def _append_built(self, value):
+        if self.built_projects is None:
+            self.built_projects = value
+        else:
+            self.built_projects += ",{}".format(value)
+        self.save()
+
+    def append_built(self, jbi):
+        if jbi.result == "FAILURE":
+            self._append_falied(jbi.projectname)
+            return True
+        if jbi.jobname.endswith("-repos"):
+            if jbi.result in ["SUCCESS", "UNSTABLE"]:
+                self._append_built(jbi.projectname)
+                return True
+        return False
+
+    @property
+    def branch_or_tag(self):
+        if self.tag:
+            return "tag/{}".format(self.tag)
+        return "branch/{}".format(self.branch)
+
+    def _next(self):
+        if self.built_projects is None:
+            return self.build_deps[0][0]
+        built_len = len(self.built_projects)
+        if built_len == len(self.projects):
+            return
+        built_list = self.built_projects_list
+        build_deps_size = len(",".join(self.config.build_deps.keys()))
+        if built_len < build_deps_size:
+            for grp in self.build_deps:
+                for prj in grp:
+                    if prj not in built_list:
+                        return prj
+        else:
+            for prj in self.projects_list:
+                if prj not in built_list:
+                    return prj
+
+    @property
+    def next(self):
+        res = self._next()
+        if res is not None:
+            if res in self.failed_projects_list:
+                logger.info("project: %s marked as failed, stop sending jobs")
+
+    @property
+    def failed_projects_list(self):
+        projects = JenkinsBuildInfo.objects.release_projects(
+            self.release, release_uuid=self.uuid, failed=True
+        )
+        if projects:
+            return [x for x in projects]
+        return []
+
+    @property
+    def build_deps(self):
+        if getattr(self, "_build_deps", None) is None:
+            self._build_deps = [
+                list(self.config.wanna_build_deps(0)),
+                list(self.config.wanna_build_deps(1)),
+            ]
+        return self._build_deps
+
+    @property
+    def config(self):
+        if getattr(self, "_config", None) is None:
+            self._config = ReleaseConfig(self.release)
+        return self._config
--- repoapi/urls.py
+++ repoapi/urls.py
@@ -1,74 +1,107 @@
 # Copyright (C) 2015 The Sipwise Team - http://sipwise.com
-
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
-from repoapi import views
+
+from build import views as build_views
 from release_dashboard.views import docker
+from repoapi import views
 
 api_patterns = [
-    url(r'^$', views.api_root, name='index'),
-    url(r'^jenkinsbuildinfo/$',
+    url(r"^$", views.api_root, name="index"),
+    url(
+        r"^jenkinsbuildinfo/$",
         views.JenkinsBuildInfoList.as_view(),
-        name='jenkinsbuildinfo-list'),
-    url(r'^jenkinsbuildinfo/(?P<pk>[0-9]+)/$',
+        name="jenkinsbuildinfo-list",
+    ),
+    url(
+        r"^jenkinsbuildinfo/(?P<pk>[0-9]+)/$",
         views.JenkinsBuildInfoDetail.as_view(),
-        name='jenkinsbuildinfo-detail'),
-    url(r'^release/$',
-        views.ReleaseList.as_view(),
-        name='release-list'),
-    url(r'^release/(?P<release>[^/]+)/$',
+        name="jenkinsbuildinfo-detail",
+    ),
+    url(r"^release/$", views.ReleaseList.as_view(), name="release-list"),
+    url(
+        r"^release/(?P<release>[^/]+)/$",
         views.ProjectList.as_view(),
-        name='project-list'),
-    url(r'^release_full/(?P<release>[^/]+)/$',
+        name="project-list",
+    ),
+    url(
+        r"^release_jobs/(?P<release_uuid>[^/]+)/$",
+        build_views.ReleaseJobs.as_view(),
+        name="release-job-list",
+    ),
+    url(
+        r"^release_jobs/(?P<release_uuid>[^/]+)/(?P<job>[^/]+)/$",
+        build_views.ReleaseJobsUUID.as_view(),
+        name="release-job-uuid-list",
+    ),
+    url(
+        r"^release_jobs_full/(?P<release_uuid>[^/]+)/$",
+        build_views.ReleaseJobsFull.as_view(),
+        name="release-job-full-list",
+    ),
+    url(
+        r"^release_full/(?P<release>[^/]+)/$",
         views.ProjectFullList.as_view(),
-        name='project-fulllist'),
-    url(r'^release/(?P<release>[^/]+)/(?P<project>[^/]+)/latest/$',
+        name="project-fulllist",
+    ),
+    url(
+        r"^release/(?P<release>[^/]+)/(?P<project>[^/]+)/latest/$",
         views.LatestUUID.as_view(),
-        name='latestuuid-list'),
-    url(r'^release/(?P<release>[^/]+)/(?P<project>[^/]+)/$',
+        name="latestuuid-list",
+    ),
+    url(
+        r"^release/(?P<release>[^/]+)/(?P<project>[^/]+)/$",
         views.ProjectUUIDList.as_view(),
-        name='projectuuid-list'),
-    url(r'^release/(?P<release>[^/]+)'
-        '/(?P<project>[^/]+)/(?P<uuid>[^/]+)/$',
+        name="projectuuid-list",
+    ),
+    url(
+        r"^release/(?P<release>[^/]+)" "/(?P<project>[^/]+)/(?P<uuid>[^/]+)/$",
         views.UUIDInfoList.as_view(),
-        name='uuidinfo-list'),
-    url(r'^docker/image/$',
+        name="uuidinfo-list",
+    ),
+    url(
+        r"^docker/image/$",
         docker.DockerImageList.as_view(),
-        name='dockerimage-list'),
-    url(r'^docker/image/(?P<pk>[0-9]+)/$',
+        name="dockerimage-list",
+    ),
+    url(
+        r"^docker/image/(?P<pk>[0-9]+)/$",
         docker.DockerImageDetail.as_view(),
-        name='dockerimage-detail'),
-    url(r'^docker/tag/(?P<pk>[0-9]+)/$',
+        name="dockerimage-detail",
+    ),
+    url(
+        r"^docker/tag/(?P<pk>[0-9]+)/$",
         docker.DockerTagDetail.as_view(),
-        name='dockertag-detail'),
-    url(r'^build/',
-        include('build.urls', namespace='build')),
+        name="dockertag-detail",
+    ),
+    url(r"^build/", include("build.urls", namespace="build")),
 ]
 
 api_patterns = format_suffix_patterns(api_patterns)
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(api_patterns)),
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
-    url(r'^docs/', views.schema_view),
-    url(r'^panel/', include('panel.urls',
-                            namespace='panel')),
-    url(r'^release_panel/',
-        include('release_dashboard.urls', namespace='release_dashboard')),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^", include(api_patterns)),
+    url(
+        r"^api-auth/",
+        include("rest_framework.urls", namespace="rest_framework"),
+    ),
+    url(r"^docs/", views.schema_view),
+    url(r"^panel/", include("panel.urls", namespace="panel")),
+    url(
+        r"^release_panel/",
+        include("release_dashboard.urls", namespace="release_dashboard"),
+    ),
 ]
