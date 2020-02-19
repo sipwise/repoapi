26dc6bcfd9739978c8997d0838c97054445af91d
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers release-copy-debs-yml job first
    * remove links to build_deps or build as a form of list of projects
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- build/tasks.py
+++ build/tasks.py
@@ -1,36 +1,56 @@
 # Copyright (C) 2017 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import absolute_import
-
 import logging
+
 from celery import shared_task
-from build.utils import trigger_build
+
 from build.models.br import BuildRelease
+from build.utils import trigger_build
+from build.utils import trigger_copy_deps
+from repoapi.celery import app
 
 logger = logging.getLogger(__name__)
 
 
+@app.task(bind=True)
+def build_release(self, pk):
+    br = BuildRelease.objects
+    try:
+        instance = br.get(id=pk)
+    except BuildRelease.DoesNotExist as exc:
+        raise self.retry(countdown=60 * 5, exc=exc)
+    url = trigger_copy_deps(
+        release=instance.release, internal=True, release_uuid=instance.uuid,
+    )
+    logger.info("%s triggered" % url)
+
+
 @shared_task(ignore_result=True)
-def build_release(uuid):
-    instance = BuildRelease.objects.get(uuid=uuid)
-    if instance.tag:
-        branch_or_tag = "tag/%s" % instance.tag
-    else:
-        branch_or_tag = "branch/%s" % instance.branch
-    for project in instance.projects_list:
-        url = trigger_build(project, instance.uuid, instance.release,
-                            trigger_branch_or_tag=branch_or_tag,
-                            trigger_distribution=instance.distribution)
-        logger.debug("%s triggered" % url)
+def build_project(pk, project):
+    try:
+        br = BuildRelease.objects.get(id=pk)
+    except BuildRelease.DoesNotExist:
+        logger.error(
+            "can't trigger %s on unknown release with id:%s", project, pk
+        )
+        return
+    url = trigger_build(
+        "{}-get-code".format(project),
+        br.uuid,
+        br.release,
+        trigger_branch_or_tag=br.branch_or_tag,
+        trigger_distribution=br.distribution,
+    )
+    logger.info("%s triggered" % url)
