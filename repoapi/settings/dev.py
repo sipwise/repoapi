347e406eebfa16bb458a3ceccbb50af12377eaf0
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers settings.RELEASE_JOBS jobs first
    * old build process on hidden /release_panel/old/
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    * rework panel.js to support both views /release/ and /release_uuid/
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

--- repoapi/settings/dev.py
+++ repoapi/settings/dev.py
@@ -1,40 +1,40 @@
 # Copyright (C) 2015 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from socket import gethostbyname
+from socket import gethostname
+
+from .test import *  # noqa
 
 # pylint: disable=W0401,W0614,C0413
-from .test import *
-
 # avoid having to hardcode an IP address
-from socket import gethostname, gethostbyname
 
-LOGGING['loggers']['release_dashboard']['level'] = \
-    os.getenv('DJANGO_LOG_LEVEL', 'DEBUG')
-
-# build app
-BUILD_KEY_AUTH = False
+LOGGING["loggers"]["repoapi"]["level"] = os.getenv(  # noqa
+    "DJANGO_LOG_LEVEL", "DEBUG"
+)
 
 # celery
-BROKER_BACKEND = 'amqp'
+BROKER_BACKEND = "amqp"
 CELERY_ALWAYS_EAGER = False
-BROKER_URL = 'amqp://guest:guest@rabbit'
-JBI_BASEDIR = os.path.join(BASE_DIR, 'jbi_files')
+BROKER_URL = "amqp://guest:guest@rabbit"
+JBI_BASEDIR = os.path.join(BASE_DIR, "jbi_files")  # noqa
 
 # Enable access when not accessing from localhost:
-ALLOWED_HOSTS = [gethostname(), gethostbyname(gethostname()), ]
+ALLOWED_HOSTS = [
+    gethostname(),
+    gethostbyname(gethostname()),
+]
 # or to manually override:
 # ALLOWED_HOSTS = ['172.17.0.3']
