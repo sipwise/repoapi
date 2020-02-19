60dc7dbc99b205cdfacc34899cf9dd03c6de58d6
Victor Seva <vseva@sipwise.com>
2020-02-19 23:05:24 +0100

    TT#43813 build: improvements
    * smart build release reading per release metadata file
    * triggers release-copy-debs-yml job first
    * remove links to build_deps or build as a form of list of projects
    * add some examples of config.yml releases only for testing
    * style changes by black pre-commit plugin to unify
      code style
    
    Change-Id: I5f581100e6c297a1cc0abc4bb1ddf978870b6936

---
+++ panel/static/panel/js/panel_release.js
@@ -0,0 +1,45 @@
+function create_new_release_job( data ) {
+  var job = data.jobname;
+  if ( $.release.release_jobs.has( job ) ) {
+    return;
+  }
+
+  $.release.release_jobs.add( job );
+  $.release[ job ] = { uuids: new Set(), failed: false,
+    interval: 5000, last_uuid: null, removed_uuids: new Set() };
+
+  var div_job = $( ".clone-release-job" ).clone();
+  div_job.removeClass( "hidden clone-release-job" );
+  div_job.attr( "id", job );
+  div_job.html( "<a href=\"" + data.job_url + data.buildnumber + "\">" + job + "</a>" );
+  /* eslint-disable-next-line no-undef */ //defined at panel.js file
+  div_job.addClass( get_class_status( "list-group-item-", status ) );
+  div_job.appendTo( "#release-job-list" );
+}
+
+/* eslint-disable-next-line no-unused-vars */ // used on templates
+function get_release_jobs() {
+
+  function successFunc( data, _textStatus, _jqXHR ) {
+    $( data ).each( function() {
+      if ( !$.release.release_jobs.has( this.jobname ) ) {
+        create_new_release_job( this );
+      }
+    } );
+  }
+
+  function errorFunc( _jqXHR, _status, error ) {
+    console.error( error );
+  }
+
+  var url = "/release_jobs/" + $.release.uuid + "/?format=json";
+  $.ajax( {
+    url: url,
+    method: "GET",
+    async: false,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: successFunc,
+    error: errorFunc
+  } );
+}
