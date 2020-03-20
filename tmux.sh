cfec495a76081e99e9ecfe086dbea1e486f71619
Victor Seva <vseva@sipwise.com>
2020-03-20 11:48:49 +0100

    TT#43813 django-pytest for testing instead of deprecated django-jenkins
    * tmux.sh support using a different tag for repoapi image
      this helps to download a tag from a review localy and
      hack
    
    Change-Id: I63f034d9d4b73f7e6eb69b0cd3ad6f731281f806

--- tmux.sh
+++ tmux.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+repoapi_tag=${1:-latest}
+repoapi_image=docker.mgm.sipwise.com/repoapi-buster:${repoapi_tag}
+
 docker run -d --rm \
 	--hostname repoapi-rabbit --name repoapi-rabbit rabbitmq:3
-docker pull docker.mgm.sipwise.com/repoapi-buster:latest
+docker pull "${repoapi_image}"
 docker run --rm -i -t --env=VAR_DIR=/code --link repoapi-rabbit:rabbit \
-	-v "$(pwd)":/code:rw docker.mgm.sipwise.com/repoapi-buster:latest tmux
+	-v "$(pwd)":/code:rw "${repoapi_image}" tmux
 docker stop repoapi-rabbit
