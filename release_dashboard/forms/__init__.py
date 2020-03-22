ee09744926d2e1302c795145f56ddb2718f16163
Victor Seva <vseva@sipwise.com>
2020-03-22 11:51:58 +0100

    TT#43813 release_dashboard: split settings per application
    Using https://django-appconf.readthedocs.io/en/latest/ helps us to
    keep configs per application
    
    Change-Id: I6dbac01d56ce1ea71d611e0e98a74a0875fe747c

--- release_dashboard/forms/__init__.py
+++ release_dashboard/forms/__init__.py
@@ -1,24 +1,21 @@
 # Copyright (C) 2017 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
+from ..conf import settings
 
-from django.conf import settings
-
-rd_settings = settings.RELEASE_DASHBOARD_SETTINGS
-trunk_projects = sorted(set(rd_settings['projects']) -
-                        set(rd_settings['abandoned']) -
-                        set(rd_settings['build_deps']))
-trunk_build_deps = sorted(set(rd_settings['build_deps']) -
-                          set(rd_settings['abandoned']))
-docker_projects = rd_settings['docker_projects']
+projects = set(settings.RELEASE_DASHBOARD_PROJECTS)
+abandoned = set(settings.RELEASE_DASHBOARD_ABANDONED)
+build_deps = set(settings.RELEASE_DASHBOARD_BUILD_DEPS)
+trunk_projects = sorted(projects - abandoned - build_deps)
+trunk_build_deps = sorted(build_deps - abandoned)
--- release_dashboard/tasks.py
+++ release_dashboard/tasks.py
@@ -1,28 +1,29 @@
 # Copyright (C) 2016 The Sipwise Team - http://sipwise.com
-
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import absolute_import
-
 import logging
+
 from celery import shared_task
-from release_dashboard.models import Project, DockerImage, DockerTag
-from django.conf import settings
+
+from .conf import settings
+from .models import DockerImage
+from .models import DockerTag
+from .models import Project
 from .utils import build
 from .utils import docker
 
 logger = logging.getLogger(__name__)
-rd_settings = settings.RELEASE_DASHBOARD_SETTINGS
 
 
 @shared_task(ignore_result=True)
@@ -35,7 +36,7 @@ def gerrit_fetch_info(projectname):
 
 @shared_task(ignore_result=True)
 def gerrit_fetch_all():
-    for project in rd_settings['projects']:
+    for project in settings.RELEASE_DASHBOARD_PROJECTS:
         gerrit_fetch_info.delay(project)
 
 
@@ -47,10 +48,8 @@ def docker_fetch_info(imagename):
         manifest, digest = docker.get_docker_manifests(image.name, tagname)
         if digest:
             DockerTag.objects.create(
-                name=tagname,
-                image=image,
-                manifests=manifest,
-                reference=digest)
+                name=tagname, image=image, manifests=manifest, reference=digest
+            )
 
 
 @shared_task(ignore_result=True)
@@ -59,8 +58,7 @@ def docker_fetch_project(projectname):
     images = docker.get_docker_repositories()
     project = Project.objects.get(name=projectname)
     for imagename in project.filter_docker_images(images):
-        image = DockerImage.objects.create(name=imagename,
-                                           project=project)
+        image = DockerImage.objects.create(name=imagename, project=project)
         logger.debug("%s created" % image)
         docker_fetch_info.delay(image.name)
 
@@ -70,11 +68,10 @@ def docker_fetch_all():
     DockerImage.objects.all().delete()
     images = docker.get_docker_repositories()
     logger.debug("images: %s" % images)
-    for projectname in rd_settings['docker_projects']:
+    for projectname in settings.RELEASE_DASHBOARD_DOCKER_PROJECTS:
         project, _ = Project.objects.get_or_create(name=projectname)
         for imagename in project.filter_docker_images(images):
-            image = DockerImage.objects.create(name=imagename,
-                                               project=project)
+            image = DockerImage.objects.create(name=imagename, project=project)
             logger.debug("%s created" % image)
             docker_fetch_info.delay(image.name)
 
